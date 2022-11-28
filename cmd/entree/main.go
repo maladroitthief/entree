0cc98fda45ad1b4bb92eb4e1dfbbbfc3d1064e24
Ian Weller <maladroitthief@gmail.com>
2022-11-27 21:16:19 -0500

    feat: Screen Manager
    - Adds ScreenManager for managing the screen size

--- cmd/entree/main.go
+++ cmd/entree/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/maladroitthief/entree/pkg/engine/core/game"
+	"github.com/maladroitthief/entree/pkg/ui/screen"
+)
 
 func main() {
-	fmt.Println("HELLO")
-	return
+	sm := screen.NewScreenManager("Entree", 800, 600)
+	sm.Update()
+
+	if err := ebiten.RunGame(&game.Game{}); err != nil {
+		log.Fatal(err)
+	}
 }
