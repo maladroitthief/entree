2b30c025c678ab152c076064c72f3dfea472d0e5
Ian Weller <maladroitthief@gmail.com>
2023-04-03 22:58:21 -0400

    feat: SpriteArray method
    - Adds a cleaner way of loading sprite sheets

---
+++ assets/sheets/sheets.go
@@ -0,0 +1,26 @@
+package sheets
+
+import (
+	"fmt"
+
+	"github.com/maladroitthief/entree/domain/sprite"
+)
+
+func SpriteArray(name string, row, columnStart, columnEnd int) []sprite.Sprite {
+	if columnStart > columnEnd {
+		panic("sprite loader")
+	}
+
+	spriteCount := columnEnd - columnStart + 1
+	results := make([]sprite.Sprite, spriteCount)
+
+	for i := 0; i < spriteCount; i++ {
+		results[i] = sprite.Sprite{
+			Name:   fmt.Sprintf("%s_%d", name, columnStart + i),
+			Row:    row,
+			Column: columnStart + i,
+		}
+	}
+
+	return results
+}
