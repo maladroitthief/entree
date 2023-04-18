40cba0358bc06aeb4e3875b4299b50040f9c1a84
Ian Weller <maladroitthief@gmail.com>
2023-04-18 08:48:56 -0400

    test: Canvas tests
    - Adds tests for the canvas struct

--- domain/canvas/canvas_test.go
+++ domain/canvas/canvas_test.go
@@ -1,7 +1,6 @@
 package canvas_test
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/maladroitthief/entree/domain/canvas"
@@ -16,15 +15,19 @@ func TestNewCanvas(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want *canvas.Canvas
 	}{
-		// TODO: Add test cases.
+		{
+			name: "default",
+			args: args{
+				x:    8,
+				y:    8,
+				size: 16,
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := canvas.NewCanvas(tt.args.x, tt.args.y, tt.args.size); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewCanvas() = %v, want %v", got, tt.want)
-			}
+			canvas.NewCanvas(tt.args.x, tt.args.y, tt.args.size)
 		})
 	}
 }
@@ -38,7 +41,13 @@ func TestCanvas_AddEntity(t *testing.T) {
 		c    *canvas.Canvas
 		args args
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Default",
+			c:    canvas.NewCanvas(0, 0, 0),
+			args: args{
+				e: &canvas.Entity{},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -51,13 +60,24 @@ func TestCanvas_Entities(t *testing.T) {
 	tests := []struct {
 		name string
 		c    *canvas.Canvas
+		len  int
 		want []*canvas.Entity
 	}{
-		// TODO: Add test cases.
+		{
+			name: "default",
+			c:    canvas.NewCanvas(0, 0, 0),
+			len:  0,
+			want: []*canvas.Entity{},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.c.Entities(); !reflect.DeepEqual(got, tt.want) {
+			for _, e := range tt.want {
+				tt.c.AddEntity(e)
+			}
+
+			got := tt.c.Entities()
+			if len(got) != tt.len {
 				t.Errorf("Canvas.Entities() = %v, want %v", got, tt.want)
 			}
 		})
